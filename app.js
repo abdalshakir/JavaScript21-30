2084b03870735ffc5d0f16bdee87f08875e25efa
Abdal Shakir <abdal_shakir7152hotmail.com>
2021-01-15 12:31:59 +0500

    complete

---
+++ app.js
@@ -0,0 +1,64 @@
+// Changing Case 
+
+// var a = prompt("Enter Your Name");
+// a = a.toUpperCase();
+// document.write(a);
+
+// var a = prompt("Enter Your Name");
+// var firstCharacter = a.slice(0,1);
+// var otherCharacter = a.slice(1);
+// firstCharacter = firstCharacter.toUpperCase();
+// otherCharacter = otherCharacter.toLowerCase();
+// var a = firstCharacter + otherCharacter;
+// document.write(a);
+
+// Strings: measuring length and extracting parts
+
+// var str = prompt("Enter Your Favourite Phone Model");
+// var numChar = str.length;
+// document.write(numChar);
+
+// var str = prompt("Enter Your Name");
+// var lastChar = str.slice(-1);
+// document.write(lastChar);
+
+// Strings: finding segments
+
+// var country = "Pakistan";
+// for(var i = 0; i < country.length; i++){
+//     if(country[i] === "n"){
+//         document.write("The index of n is: " + i)
+//     }
+// }
+
+// Strings: finding a character at a location
+
+// var country = "Pakistan";
+// document.write(country.charAt(3));
+
+// Strings: replacing characters
+
+// var city = "Hyderabad";
+// var cityChange = city.replace("Hyder", "Islam");
+// document.write(city + " change into " + cityChange);
+
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// var messageChange = message.replace(/and/g, "&");
+// document.write(messageChange);
+
+// var aNum = +prompt("Enter a positive integer");
+// // var roundNum = Math.round(aNum);
+// // document.write(roundNum);
+// var ceilNum = Math.ceil(aNum);
+// document.write(ceilNum);
+// var floorNum = Math.floor(aNum);
+// document.write(floorNum);
+
+// var aNum = +prompt("Enter a negative floating point number");
+// var roundNum = Math.round(aNum);
+// document.write(roundNum);
+// var ceilNum = Math.ceil(aNum);
+// document.write(ceilNum);
+// var floorNum = Math.floor(aNum);
+// document.write(floorNum);
+
