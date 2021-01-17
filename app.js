e305dd50f1e6e6cb7b6e91111f6489118393e885
Abdal Shakir <abdal_shakir7152hotmail.com>
2021-01-18 04:10:30 +0500

    complete app.js

--- app.js
+++ app.js
@@ -62,3 +62,54 @@
 // var floorNum = Math.floor(aNum);
 // document.write(floorNum);
 
+// Generating random numbers
+
+// var diceValue = Math.random();
+// document.write(diceValue);
+
+// var toss = Math.random()*2;
+// var tossFloor = Math.floor(toss);
+// if(tossFloor === 1){
+//     document.write("<h1>Heads</h>");
+// }
+// else{
+//     document.write("<h1>Tails</h1>");
+// }
+
+// var guessNum = Math.random()*10;
+// var guessNumCeil = Math.ceil(guessNum);
+// var guessUserNum = prompt("Enter a number from 1 to 10")
+// if(guessUserNum == guessNumCeil){
+//     document.write("Congratulations");
+// }
+// else{
+//     document.write("Try Again");
+// }
+
+// Converting strings to integers and decimals
+
+// var weight = prompt("Enter your weight");
+// var weightA = parseInt(weight);
+// document.write(weightA + "<br />");
+// var weightB = parseInt(weight) + "kgs";
+// document.write(weightB + "<br />");
+// var weightC = parseFloat(weight) + "kgs";
+// document.write(weightC + "<br />");
+// var weightD = parseFloat(weight) + "kilograms";
+// document.write(weightD);
+
+// Converting strings to numbers, numbers to strings
+
+// var str = "421";
+// var num = Number(str);
+// document.write(num + "<br />" + typeof num);
+
+// var num = 35.36;
+// var str = num.toString();
+// document.write(str);
+
+// Controlling the length of decimals
+
+// var percentage = 30 / 45 * 100;
+// var decimal = percentage.toFixed(2);
+// document.write(decimal);
